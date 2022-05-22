16d258fe6dee694eea3ad93849481c85eb7c2004
winwin2011 <yjh2011@live.com>
2022-05-22 17:41:09 +0800

    chore: app build out dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     port: 4000,
+  },
+  build: {
+    outDir: 'dist/app',
   },
 })
