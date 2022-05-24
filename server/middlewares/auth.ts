13777d8be3e4a1f682546de1a3cc5236185dd39f
winwin2011 <yjh2011@live.com>
2022-05-25 06:43:41 +0800

    fix: remove

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,7 +1,6 @@
 import { createAuth } from '@winwin/koa-authentication'
 export const auth = createAuth({
   verify(username, password) {
-    console.log({ username, password })
     return username === process.env.USERNAME
      && password === process.env.PASSWORD
   },
