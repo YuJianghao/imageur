85f82061b2e0c1e45cc9004af552ce6f2fd7373c
winwin2011 <yjh2011@live.com>
2022-05-27 21:57:40 +0800

    chore: base vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+  plugins: [vue()],
   server: {
     port: 4000,
     proxy: {
