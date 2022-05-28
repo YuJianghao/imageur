493ae777e61a73ea518588b3de0fb20887625ca4
winwin2011 <yjh2011@live.com>
2022-05-28 20:52:36 +0800

    chore: uno css

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { router } from './router'
 import App from './App.vue'
 
+import '@unocss/reset/tailwind.css'
+import 'uno.css'
+
 createApp(App)
   .use(router)
   .mount('#app')
---
+++ app/pages/ui.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TextField from '@/ui/TextField.vue'
+</script>
+
+<template>
+  <div flex="~ col gap2" w-full>
+    <TextField />
+    <TextField />
+  </div>
+</template>
