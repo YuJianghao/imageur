493ac0385ffbf5ae3d853725f119e4b51b4a198f
winwin2011 <yjh2011@live.com>
2022-06-11 13:11:23 +0800

    chore: lint

--- server/index.ts
+++ server/index.ts
@@ -4,8 +4,8 @@ import { auth } from '@server/middlewares/auth'
 import sts from '@server/router/sts'
 import { statics } from './middlewares/statics'
 import Koa from 'koa'
-import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import bodyParser from 'koa-bodyparser'
 const app = new Koa()
 
 app.use(logger())
