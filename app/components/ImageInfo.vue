4efb3f3082512ee3936123d9f0d6290ea40b0869
winwin2011 <yjh2011@live.com>
2022-06-11 12:18:11 +0800

    chore: ui

---
+++ app/components/ImageInfo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { NButton, NIcon, NImage, NText } from 'naive-ui'
+import { LinkOutline, LogoMarkdown } from '@vicons/ionicons5'
+defineProps<{
+  name: string
+  url: string
+}>()
+</script>
+
+<template>
+  <div flex items-center gap-2 mt2>
+    <NImage :src="url" width="50" height="50" object-fit="cover" />
+    <NText flex-1>
+      {{ name }}
+    </NText>
+    <NButton>
+      <template #icon>
+        <NIcon>
+          <LinkOutline />
+        </NIcon>
+      </template>
+    </NButton>
+    <NButton>
+      <template #icon>
+        <NIcon>
+          <LogoMarkdown />
+        </NIcon>
+      </template>
+    </NButton>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,47 +5,64 @@ import { ImageOutline as UploadIcon } from '@vicons/ionicons5'
 import type { FileInfo } from 'naive-ui/es/upload/src/interface'
 import { signOut } from '~/api/auth'
 import { useOss } from '~/composables/oss'
+import ImageInfo from '~/components/ImageInfo.vue'
 const oss = useOss()
-const imgs = ref<{ name: string; url: string }[]>([])
+const imgs = ref<{ name: string; url: string }[]>(
+  [{
+    name: 'share/a_ddV_wNDNxKQetCVRWw9',
+    url: 'http://winwincdn.oss-cn-beijing.aliyuncs.com/share/a_ddV_wNDNxKQetCVRWw9',
+  },
+  {
+    name: 'share/SE69TzOJMmXzrsCr3ckvs',
+    url: 'http://winwincdn.oss-cn-beijing.aliyuncs.com/share/SE69TzOJMmXzrsCr3ckvs',
+  }],
+)
 const vars = useThemeVars()
 const fileInfo = ref<FileInfo>()
 const onUploadChange = (options: { file: FileInfo }) => (fileInfo.value = options.file)
+const uploadEl = ref()
 const upload = () => {
   const file = fileInfo.value?.file
   if (!file)
     return
   oss.upload(file).then(({ name, url }) => {
     imgs.value.push({ name, url })
+    uploadEl.value?.clear()
+    fileInfo.value = undefined
   })
 }
 </script>
 
 <template>
-  <div h-100vh w-100vw :style="{ backgroundColor: vars.primaryColor }">
+  <div h-100vh w-100vw flex="~ col" :style="{ backgroundColor: vars.primaryColor }">
     <nav flex="~" py2 px4>
       <div flex-1 />
       <NText secondary text text-white:50 hover:text-white cursor-pointer @click="signOut">
         SignOff
       </NText>
     </nav>
-    <NCard w150 mx-auto mt-50>
-      <NUpload :max="1" :on-change="onUploadChange">
-        <NUploadDragger>
-          <div style="margin-bottom: 12px">
-            <NIcon size="48" :depth="3">
-              <UploadIcon />
-            </NIcon>
-          </div>
-          <NText style="font-size: 16px">
-            Click our Drag inside to upload.
-          </NText>
-        </NUploadDragger>
-      </NUpload>
-      <NButton block :disabled="!fileInfo" @click="upload">
-        upload
-      </NButton>
-      <pre>{{ imgs }}</pre>
-    </NCard>
+    <div flex-1 flex items-center pb-10>
+      <div max-h-80vh overflow-auto w150 mx-auto>
+        <NCard>
+          <NUpload ref="uploadEl" :max="1" :on-change="onUploadChange" list-type="image">
+            <NUploadDragger>
+              <div style="margin-bottom: 12px">
+                <NIcon size="48" :depth="3">
+                  <UploadIcon />
+                </NIcon>
+              </div>
+              <NText style="font-size: 16px">
+                Click our drag inside to upload.
+              </NText>
+            </NUploadDragger>
+          </NUpload>
+          <NButton block :disabled="!fileInfo" @click="upload">
+            upload
+          </NButton>
+          <ImageInfo v-for="item in imgs" :key="item.url" :name="item.name" :url="item.url" />
+        </NCard>
+      </div>
+    </div>
   </div>
 </template>
 
