22d96b9241f853d8310ed8e2c8c059d557232dfc
winwin2011 <yjh2011@live.com>
2022-06-11 12:53:37 +0800

    feat: statics

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import './boot'
+import path from 'path'
 import { auth } from '@server/middlewares/auth'
 import sts from '@server/router/sts'
+import { statics } from './middlewares/statics'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
@@ -9,6 +11,8 @@ const app = new Koa()
 app.use(logger())
 app.use(bodyParser())
 app.use(auth.router.routes())
+
+app.use(statics(path.resolve('./dist/app')))
 
 app.use(auth.auth)
 
